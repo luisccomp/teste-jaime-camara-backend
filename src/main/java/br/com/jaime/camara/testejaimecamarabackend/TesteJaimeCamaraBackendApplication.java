85b0273db67f36159d14ef2b5aae4dff19975fdf
Luis Carlos Silva Junior <luisccomp@gmail.com>
2024-01-23 23:43:01 -0300

    🎄Estrutura inicial criada

---
+++ src/main/java/br/com/jaime/camara/testejaimecamarabackend/TesteJaimeCamaraBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.jaime.camara.testejaimecamarabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteJaimeCamaraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteJaimeCamaraBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jaime/camara/testejaimecamarabackend/TesteJaimeCamaraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jaime.camara.testejaimecamarabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteJaimeCamaraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
